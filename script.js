f91635e65f6e1facec27c77a198f3463bed307aa
augustosoaresluiz-hue <augusto.soares.luiz@escola.pr.gov.br>
2026-08-28 07:52:59 -0300

    kuytkuykut

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.querySelector("button");
-botao.addEventListener("click", botaoclicado)
+botao.addEventListener("click", botaoclicado);
 
 function botaoClicado() {
     let texto = botao.querySelector("span");
